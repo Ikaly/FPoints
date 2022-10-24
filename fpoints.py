164d6b70b1a7dacc6b422c646d9d10a36e86b0f8
Ikaly <dummydumm33@gmail.com>
2022-10-24 22:02:30 +0200

    Update for 2022

--- fpoints.py
+++ fpoints.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import glob
 import re
@@ -8,17 +8,16 @@ import os
 def read_points(file):
     if os.path.isfile(file):
         with open(file, "r") as f:
-            s = re.search(r"Punkte gesamt: ?(\d+)", f.read(), re.IGNORECASE)
+            s = re.search(r"Total score: ?(\d+)", f.read(), re.IGNORECASE)
             return int(s.group(1)) if s else 0
     return 0
 
 
 if __name__ == '__main__':
-    results = glob.glob("*.out")
-    filename = re.compile("(.*)(_1\.out)")
-
+    results = glob.glob("**/*.out*", recursive=True)
+    filename = re.compile("(.*)(\.out)_[12]+")
     tasks = map(lambda f: filename.search(f).group(1), filter(filename.match, results))
-    points = map(lambda f: (read_points(f + "_1.out") + read_points(f + "_2.out")) / (2 if os.path.isfile(f + "_2.out")
-                                                                                      else 1), tasks)
+    points = map(lambda f: (read_points(f + ".out_1") + read_points(f + ".out_2")) / (2 if os.path.isfile(f + ".out_2")
+                                                                                          else 1), tasks)
 
     print(str(sum(points)) + " / 500")
